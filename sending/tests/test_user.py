5e0c76e8ce84552aaaaa37251d980e57f7e050ef
ayslanleal <ayslanleal15@gmail.com>
2022-02-07 19:51:02 -0300

    Testing fixtures

--- sending/tests/test_user.py
+++ sending/tests/test_user.py
@@ -1,32 +1,36 @@
 import imp
+from pickletools import pyset
+
+import pytest
 from sending.spam import Connect, Session, User
 
 
+@pytest.fixture
+def connect():
+    connect_obj = Connect()
+    yield connect_obj
+    connect_obj.close()
 
+@pytest.fixture
+def session(connect):
+    session_init = connect.init_session()
+    yield session_init
+    session_init.close()
+    session_init.roll_back()
 
-
-def test_save_user():
-    conection = Connect()
-    session = conection.init_session()
+def test_save_user(session):
     user =  User(name='Ney')
     session.save(user)
     assert isinstance(user.id, int)
-    session.roll_back()
-    session.close()
-    conection.close()
 
 
-def test_show_users():
-    conection = Connect()
-    session = conection.init_session()
+def test_show_users(connect,session):
     users = [User(name="David"), User(name="Leal")]
     for user in users:
         session.save(user)
 
     assert users == session.show()
-    session.roll_back()
-    session.close()
-    conection.close()
+
 
 
 
