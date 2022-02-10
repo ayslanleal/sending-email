5a9333bf68bdb78484e5b35adee191a0141937b3
ayslanleal <ayslanleal15@gmail.com>
2022-02-09 21:39:05 -0300

    Testing mock

--- sending/tests/send_users_base.py
+++ sending/tests/send_users_base.py
@@ -3,16 +3,7 @@ import pytest
 from sending.spam.sending_email import Sending
 from sending.spam.main import SendingSpam
 from sending.spam.sending_email import User
-
-class MockSend(Sending):
-    def __init__(self):
-        super().__init__()
-        self.qtd_sending_email = 0
-        self.param_send = None
-
-    def send(self, sender, to, title, body):
-        self.param_send = (sender, to, title, body)
-        self.qtd_sending_email += 1
+from unittest.mock import Mock
 
 @pytest.mark.parametrize(
     'users', [
@@ -28,7 +19,7 @@ class MockSend(Sending):
 def test_send_span(session, users):
     for user in users:
         session.save(user)
-    sending = Sending()
+    sending = Mock()
     sending_spam = SendingSpam(session, sending)
     sending_spam.send_emails(
         'foobar@gmail.com',
@@ -36,12 +27,12 @@ def test_send_span(session, users):
         'Lista de notas'
     )
 
-    assert len(users) == sending.qtd_sending_email()
+    assert len(users) == sending.send.call_count
 
 def test_param_span(session, users):
     user = User(name="David", email="davidayslan10@outlook.com")
     session.save(user)
-    sending = Sending()
+    sending = Mock()
     sending_spam = SendingSpam(session, sending)
     sending_spam.send_emails(
         'foobar@gmail.com',
@@ -50,4 +41,4 @@ def test_param_span(session, users):
         'Lista de notas'
     )
 
-    assert sending.param_send == ('foobar@gmail.com','Notas fiscas','Lista de notas')
+    sending.param_send.assert_called_once_with('foobar@gmail.com','Notas fiscas','Lista de notas')
